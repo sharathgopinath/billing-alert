656dc6b063476cae3268c706dbd058cafb40c447
sharath <sharathgopinathrao@gmail.com>
2020-06-28 20:27:07 +1000

    Initial commit

---
+++ tests/Consumer.Tests/BillingMonitorFunctionTests.cs
@@ -0,0 +1,22 @@
+using Xunit;
+using Amazon.Lambda.TestUtilities;
+
+using BillingMonitor;
+
+namespace Consumer.Tests
+{
+    public class FunctionTest
+    {
+        [Fact]
+        public void TestToUpperFunction()
+        {
+
+            // Invoke the lambda function and confirm the string was upper cased.
+            var function = new Function();
+            var context = new TestLambdaContext();
+            var upperCase = function.FunctionHandler("hello world", context);
+
+            Assert.Equal("HELLO WORLD", upperCase);
+        }
+    }
+}
