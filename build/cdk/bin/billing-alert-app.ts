903e6537ebef80da12f81a2d0fb84b1e414827ba
sharath <sharathgopinathrao@gmail.com>
2020-07-19 14:39:50 +1000

    fix gitgnore

---
+++ build/cdk/bin/billing-alert-app.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { BillingAlertAppStack } from '../lib/billing-alert-app-stack';
+import context from '../helpers/context';
+
+const app = new cdk.App();
+const stackName = `${context.getAppName(app)}-stack`;
+new BillingAlertAppStack(app, 'BillingAlertAppStack', {
+    stackName: stackName,
+    tags:{
+        'AppName': context.getAppName(app),
+        'StackName': stackName
+    }
+});
